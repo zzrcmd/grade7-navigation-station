9d968fd39a87d34e9c96f0e8bbc0deca47097698
zzrcmd <123740674+zzrcmd@users.noreply.github.com>
2025-03-25 05:11:15 +0000

    测试

--- push.sh
+++ push.sh
@@ -1,11 +1,16 @@
-# 拉取远程分区
+#!/bin/bash
+
+# 拉取远程仓库的更新
 git pull origin main
 
 # 添加所有更改到暂存区
 git add -A
 
+# 提示用户输入提交描述
+read -p "请输入提交描述: " commit_message
+
 # 提交暂存区的更改
-git commit -m "Add nodejs_compat to wrangler.toml"
+git commit -m "$commit_message"
 
 # 推送到远程仓库的 main 分支
 git push origin main
