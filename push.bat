d2dd41b8a83175b617ea0c7fa99af7acdaf008f0
unknown <zzrcmd@163.com>
2025-03-28 22:55:35 +0800

    1

--- push.bat
+++ push.bat
@@ -1,17 +1,17 @@
 @echo off
 
-echo 拉取远程仓库的更新...
+echo 拉取远程仓库的更新
 git pull origin main
 
-echo 添加所有更改到暂存区...
+echo 添加所有更改到暂存区
 git add -A
 
-set /p commit_message="请输入提交描述: "
+set /p commit_message="请输入提交描述："
 
-echo 提交暂存区的更改...
+echo 提交暂存区的更改
 git commit -m "%commit_message%"
 
-echo 推送到远程仓库的 main 分支...
+echo 推送到远程仓库的 main 分支
 git push origin main
 
 echo 完成!
